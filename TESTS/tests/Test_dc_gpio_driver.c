3737c9b24d951d66019e16abf3f1ab8c8a2081cb
brainmorph <capota.d@gmail.com>
2020-11-23 11:30:37 -0600

    Clean up gpio driver test file

--- TESTS/tests/Test_dc_gpio_driver.c
+++ TESTS/tests/Test_dc_gpio_driver.c
@@ -1,9 +1,25 @@
+/***************************************************************************************************
+*     Include Files
+***************************************************************************************************/
 #include "dc_gpio_driver.h"
 #include "unity.h"
 #include "unity_fixture.h"
 
 
+/***************************************************************************************************
+*     Local Macro Definitions
+***************************************************************************************************/
+#define CHECK_BIT(reg, num) ((reg >> num) & 0x1)
+
+/***************************************************************************************************
+*     Static Function Declarations
+***************************************************************************************************/
+
+/***************************************************************************************************
+*     Static Variable Definitions
+***************************************************************************************************/
 static uint32_t virtual_register = 0;
+
 
 
 TEST_GROUP(dc_gpio_driver);
@@ -22,49 +38,50 @@ TEST_TEAR_DOWN(dc_gpio_driver)
 
 TEST(dc_gpio_driver, SetBit)
 {
+	// This test proves that without calling SetBit(), the bit doesn't set (obvious but useful)
 	uint8_t bitToTest = 7;
+	TEST_ASSERT_NOT_EQUAL(1, CHECK_BIT(virtual_register, bitToTest));
+
+	// Now confirm that SetBit() does its job
+	bitToTest = 7;
 	DGD_SetBit(&virtual_register, bitToTest);
-	uint8_t temp = (virtual_register >> bitToTest) & 0x1; // read correct bit number
-	TEST_ASSERT_EQUAL(1, temp);
+	TEST_ASSERT_EQUAL(1, CHECK_BIT(virtual_register, bitToTest));
 
 	bitToTest = 0;
 	DGD_SetBit(&virtual_register, bitToTest);
-	temp = (virtual_register >> bitToTest) & 0x1; // read correct bit number
-	TEST_ASSERT_EQUAL(1, temp);
+	TEST_ASSERT_EQUAL(1, CHECK_BIT(virtual_register, bitToTest));
 
 	bitToTest = 31;
 	DGD_SetBit(&virtual_register, bitToTest);
-	temp = (virtual_register >> bitToTest) & 0x1; // read correct bit number
-	TEST_ASSERT_EQUAL(1, temp);
+	TEST_ASSERT_EQUAL(1, CHECK_BIT(virtual_register, bitToTest));
 }
 
 TEST(dc_gpio_driver, ClearBit)
 {
-	/* NOTE: make sure SetBit() is tested and working before running these tests! */
+	/***
+	 * NOTE: make sure DGD_SetBit() is tested and working before running these tests!
+	 ***/
 
+	// This test proves that without calling ClearBit(), the bit doesn't clear (obvious but useful)
 	uint8_t bitToTest = 7;
 	DGD_SetBit(&virtual_register, bitToTest);
-	//DGD_ClearBit(&virtual_register, bitToTest);
-	uint8_t temp = (virtual_register >> bitToTest) & 0x1; // read correct bit number
-	TEST_ASSERT_NOT_EQUAL(0, temp);
+	TEST_ASSERT_NOT_EQUAL(0, CHECK_BIT(virtual_register, bitToTest)); // notice the NOT equal in the test
 
+	// Now confirm that ClearBit() does its job
 	bitToTest = 7;
 	DGD_SetBit(&virtual_register, bitToTest);
 	DGD_ClearBit(&virtual_register, bitToTest);
-	temp = (virtual_register >> bitToTest) & 0x1; // read correct bit number
-	TEST_ASSERT_EQUAL(0, temp);
+	TEST_ASSERT_EQUAL(0, CHECK_BIT(virtual_register, bitToTest));
 
 	bitToTest = 0;
 	DGD_SetBit(&virtual_register, bitToTest);
 	DGD_ClearBit(&virtual_register, bitToTest);
-	temp = (virtual_register >> bitToTest) & 0x1; // read correct bit number
-	TEST_ASSERT_EQUAL(0, temp);
+	TEST_ASSERT_EQUAL(0, CHECK_BIT(virtual_register, bitToTest));
 
 	bitToTest = 31;
 	DGD_SetBit(&virtual_register, bitToTest);
 	DGD_ClearBit(&virtual_register, bitToTest);
-	temp = (virtual_register >> bitToTest) & 0x1; // read correct bit number
-	TEST_ASSERT_EQUAL(0, temp);
+	TEST_ASSERT_EQUAL(0, CHECK_BIT(virtual_register, bitToTest));
 }
 
 TEST(dc_gpio_driver, InitializeGPIOAPort)
--- TESTS/tests/test_runners/TestRunner_dc_gpio_driver.c
+++ TESTS/tests/test_runners/TestRunner_dc_gpio_driver.c
@@ -7,5 +7,5 @@ TEST_GROUP_RUNNER(dc_gpio_driver)
 {
 	RUN_TEST_CASE(dc_gpio_driver, InitializeGPIOAPort);
 	RUN_TEST_CASE(dc_gpio_driver, SetBit);
-	RUN_TEST_CASE(dc_gpio_driver, ClearBit);
+	RUN_TEST_CASE(dc_gpio_driver, ClearBit); /* This test runner MUST get called after the SetBit test runner */
 }
